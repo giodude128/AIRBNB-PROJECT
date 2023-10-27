d9f05e19454baf901d4a92074c54f8a379a10cb4
Your Name <you@example.com>
2023-10-27 16:23:04 -0700

    phase 5 done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,15 +33,16 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+        const { email, password, username} = req.body;
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({ email, username, hashedPassword });
+
 
       const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
-      };
+    };
 
       await setTokenCookie(res, safeUser);
 
@@ -53,5 +54,5 @@ router.post(
 
 
 
-  
+
 module.exports = router;
