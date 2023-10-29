8ea460f7bc3d1e7099be21cd594f7fa540865e2e
Your Name <you@example.com>
2023-10-29 15:11:17 -0700

    edited

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -80,9 +80,11 @@ router.get(
       if (user) {
         const safeUser = {
           id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           username: user.username,
-        };
+      };
         return res.json({
           user: safeUser
         });
