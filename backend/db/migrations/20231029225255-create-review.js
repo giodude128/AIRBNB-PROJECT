76de79828ff89afd01172dab69ad82eacd4de893
Your Name <you@example.com>
2023-10-29 21:11:51 -0700

    cascade

--- backend/db/migrations/20231029225255-create-review.js
+++ backend/db/migrations/20231029225255-create-review.js
@@ -1,45 +1,50 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+/** @type {import('sequelize').QueryInterface} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Reviews', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       spotId: {
         type: Sequelize.INTEGER,
         references: {
-          model: "Spots"
-        }
+          model: "Spots",
+          key: "id",
+          onDelete: "CASCADE", // Add CASCADE option here
+        },
       },
       userId: {
         type: Sequelize.INTEGER,
         references: {
-          model: "Users"
-        }
+          model: "Users",
+          key: "id",
+          onDelete: "CASCADE", // Add CASCADE option here
+        },
       },
       review: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       stars: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Reviews');
-  }
+  },
 };
