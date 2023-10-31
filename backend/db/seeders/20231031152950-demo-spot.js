828368c9d993a303acba719a4396de5575636063
Your Name <you@example.com>
2023-10-31 10:58:54 -0700

    demospoterror

--- backend/db/seeders/20231031152950-demo-spot.js
+++ backend/db/seeders/20231031152950-demo-spot.js
@@ -14,7 +14,7 @@ module.exports = {
         country: 'USA',
         lat: 23.56,
         lng: 56.34,
-        name: 'GIO',
+        name: 'Gio',
         description: 'somewhere',
         price: 850000
       },
@@ -49,7 +49,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      ownerId: { [Op.in]: ['1', '2', '3'] }
+      name: { [Op.in]: ['Gio', 'Light', 'Z'] }
     }, {});
   }
 };
