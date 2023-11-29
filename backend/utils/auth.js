b3382c337226b487d7a7c596f0544185ce7ba76a
Your Name <you@example.com>
2023-11-29 10:16:41 -0800

    fix3

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,71 +1,63 @@
-// backend/utils/auth.js
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
-
-// Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-    // CREATE TOKEN
+    // Create the token.
     const safeUser = {
-      id: user.id,
-      email: user.email,
-      username: user.username,
+        id: user.id,
+        email: user.email,
+        username: user.username,
     };
     const token = jwt.sign(
-      { data: safeUser },
-      secret,
-      { expiresIn: parseInt(expiresIn) }
+        { data: safeUser },
+        secret,
+        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
 
     const isProduction = process.env.NODE_ENV === "production";
 
-    // TOKEN COOKIE
+    // Set the token cookie
     res.cookie('token', token, {
-      maxAge: expiresIn * 1000,
-      httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction && "Lax"
+        maxAge: expiresIn * 1000, // maxAge in milliseconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
     });
 
     return token;
-  };
-
-
+};
 
 const restoreUser = (req, res, next) => {
-    // PARSING TOKEN
+    // token parsed from cookies
     const { token } = req.cookies;
     req.user = null;
 
     return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-      if (err) {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.findByPk(id, {
+                attributes: {
+                    include: ['email', 'createdAt', 'updatedAt']
+                }
+            });
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
         return next();
-      }
-
-      try {
-        const { id } = jwtPayload.data;
-        req.user = await User.findByPk(id, {
-          attributes: {
-            include: ['email', 'createdAt', 'updatedAt']
-          }
-        });
-      } catch (e) {
-        res.clearCookie('token');
-        return next();
-      }
-
-      if (!req.user) res.clearCookie('token');
-
-      return next();
     });
-  };
+};
 
-
-
-// If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
@@ -74,8 +66,6 @@ const requireAuth = function (req, _res, next) {
     err.errors = { message: 'Authentication required' };
     err.status = 401;
     return next(err);
-  }
-
-
+}
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
